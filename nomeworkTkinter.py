2f8b7de93dc7d24c99654fe17b84bb8c66fbadb1
nickolas-maslow <99341072+nickolas-maslow@users.noreply.github.com>
2022-06-06 21:12:11 +0600

    homework

--- nomeworkTkinter.py
+++ nomeworkTkinter.py
@@ -3,22 +3,22 @@ from tkinter import ttk
 
 
 def alright():
-    print("Everything is fine.")
+    print("Все работает хорошо.")
 
 
 root = Tk()  # создание объекта ткинтер
 
 frm = ttk.Frame(root, padding=10)  # создание главного окна
-root.title("big button")
+root.title("Большая кнопка")
 root.geometry("640x480")  # размер окна
-frm.grid()  # cетка
+frm.pack()
 
-Button(text="Button",  # текст кнопки
-       background="#000000",  # фон кнопки
-       foreground="#FFFFFF",  # цвет текста
+Button(text="Эта кнопка",  # текст кнопки
+       background="grey",  # фон кнопки
+       foreground="black",  # цвет текста
        padx="20",  # отступ по горизонтали
        pady="8",  # отступ по вертикали
        font="16",  # высота шрифта
-       command=alright).grid(column=3, row=5)
+       command=alright).pack()
 
 root.mainloop()
